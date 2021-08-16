f45ae5b52560c2de712d9ec7f4b075505cc4e6cc
lenyn15 <you@example.com>
2021-08-16 17:40:50 -0500

    Proyecto subido

---
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.recyclerview.app_citas_medicas.view.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.recyclerview.app_citas_medicas.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
