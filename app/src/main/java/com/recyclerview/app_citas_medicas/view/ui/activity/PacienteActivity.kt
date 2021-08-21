c6038a1215eb6b769bbe75cd7b5032b8bc3131ad
aldairDiazLopez97 <aldair14101997@gmail.com>
2021-08-21 02:01:50 +0200

    Activity Contenedor de los fragments principales para paciente

---
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/PacienteActivity.kt
@@ -0,0 +1,11 @@
+package com.recyclerview.app_citas_medicas.view.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PacienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paciente)
+    }
+}
