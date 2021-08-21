997473cd8051f338c5266fc050a854906220d5d0
aldairDiazLopez97 <aldair14101997@gmail.com>
2021-08-21 19:52:59 +0200

    Agregue la barra de navegacion

--- app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/PacienteActivity.kt
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/PacienteActivity.kt
@@ -2,10 +2,28 @@ package com.recyclerview.app_citas_medicas.view.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.recyclerview.app_citas_medicas.R
 
 class PacienteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paciente)
+
+        // Para la navegacion
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView) // el fragmento de la actividad paciente
+
+        // PAra el bar
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.citasFragment, R.id.ubicacionFragment, R.id.profileFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+
     }
 }
