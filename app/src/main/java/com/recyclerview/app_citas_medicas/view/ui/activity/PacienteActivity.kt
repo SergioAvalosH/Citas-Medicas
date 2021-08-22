6f4d68a531e2fa8920ca373f729411c17d74076d
aldairDiazLopez97 <aldair14101997@gmail.com>
2021-08-22 09:21:00 +0200

    agregue unos metodos al activity paciente

--- app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/PacienteActivity.kt
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/PacienteActivity.kt
@@ -6,10 +6,19 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.recyclerview.app_citas_medicas.R
 
-class PacienteActivity : AppCompatActivity() {
+class PacienteActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map:GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paciente)
@@ -22,6 +31,28 @@ class PacienteActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController,appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController) // IControlador de l navegation
 
+        // Para google Map - Agregarlo de forma dinamica
+        val mapFragment = SupportMapFragment.newInstance()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.ubicacionFragment, mapFragment)
+            .commit()
 
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+    private fun createMarker(){
+        val coordinates = LatLng(-9.075513, -78.602313)
+        val marker = MarkerOptions().position(coordinates).title("Hospital la Caleta")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates,18f),
+            4000,
+            null
+
+        )
     }
 }
