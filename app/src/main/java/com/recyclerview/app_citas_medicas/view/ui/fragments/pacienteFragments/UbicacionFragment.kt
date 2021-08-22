6d2287c1ad045f91712009707d29ab8a864d265f
aldairDiazLopez97 <aldair14101997@gmail.com>
2021-08-22 08:26:15 +0200

    solo poco

--- app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/fragments/pacienteFragments/UbicacionFragment.kt
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/fragments/pacienteFragments/UbicacionFragment.kt
@@ -7,20 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import com.recyclerview.app_citas_medicas.R
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class UbicacionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -30,13 +20,20 @@ class UbicacionFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ubicacion, container, false)
     }
-
+    /*
     companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
+         * @param param1 Parame
+
+        de:	Aldair Leonardo Diaz Lopez <aldair14101997@gmail.com>
+        para:	Stefanía Cajusol <mili.cajusol@gmail.com>
+        fecha:	22 ago. 2021 05:31
+        asunto:	Re: Sanado y salvado por si te preguntabas
+        enviado por:	gmail.com
+        ter 1.
          * @param param2 Parameter 2.
          * @return A new instance of fragment UbicacionFragment.
          */
@@ -49,5 +46,5 @@ class UbicacionFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
+    }*/
 }
