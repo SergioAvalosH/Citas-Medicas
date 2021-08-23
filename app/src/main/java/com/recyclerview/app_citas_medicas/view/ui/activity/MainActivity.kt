8f480622a1901b0df0d5385ccd48aaaf7e86f424
XCDarkingCX <eucebiocardenasmeza2019@gmail.com>
2021-08-23 00:01:28 -0500

    PruebaP

--- app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/MainActivity.kt
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/MainActivity.kt
@@ -2,12 +2,23 @@ package com.recyclerview.app_citas_medicas.view.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentResultListener
+import androidx.fragment.app.FragmentTransaction
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.recyclerview.app_citas_medicas.R
+import com.recyclerview.app_citas_medicas.view.ui.fragments.IndiceFragment
+import com.recyclerview.app_citas_medicas.view.ui.fragments.LoginAdministradorFragment
+import kotlinx.android.synthetic.main.fragment_indice.*
 
 class MainActivity : AppCompatActivity() {
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_paciente)
+        setContentView(R.layout.fragment_indice)
+        val fragmentInicio = IndiceFragment()
+        val fragmentAdmin = LoginAdministradorFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.tvAdmin,fragmentInicio).commit()
 
     }
 }
