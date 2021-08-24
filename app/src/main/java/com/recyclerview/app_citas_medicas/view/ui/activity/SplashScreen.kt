59d4f6f1ada7fa983f174408be4c82ed2f110f17
XCDarkingCX <eucebiocardenasmeza2019@gmail.com>
2021-08-24 02:30:50 -0500

    PruebaP

---
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/SplashScreen.kt
@@ -0,0 +1,13 @@
+package com.recyclerview.app_citas_medicas.view.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.recyclerview.app_citas_medicas.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
