599cc98623fcd716e5ae1fdfe668273f5afadcab
XCDarkingCX <eucebiocardenasmeza2019@gmail.com>
2021-08-24 03:24:18 -0500

    PruebaP

---
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/RegisterPacient.kt
@@ -0,0 +1,36 @@
+package com.recyclerview.app_citas_medicas.view.ui.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.recyclerview.app_citas_medicas.R
+import kotlinx.android.synthetic.main.activity_register_pacient.*
+
+class RegisterPacient : AppCompatActivity() {
+    val db = Firebase.firestore
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_pacient)
+        tvPRegistrar.setOnClickListener{
+            db.collection("usuarios").document(tvPDNI.text.toString()).set(
+                hashMapOf(
+                    "DNI" to tvPDNI.text.toString(),
+                    "Nombres" to tvPName.text.toString(),
+                    "Apellidos" to tvPSurname.text.toString(),
+                    "Contraseña" to  tvPPassword.text.toString(),
+                    "Tipo" to "Paciente"
+                )
+            )
+            tvPDNI.text.clear()
+            tvPName.text.clear()
+            tvPSurname.text.clear()
+            tvPPassword.text.clear()
+        }
+        tvVolver.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
