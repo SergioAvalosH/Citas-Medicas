b76dd6a2175e81054482c319ce20391ce379be91
aldairDiazLopez97 <aldair14101997@gmail.com>
2021-08-25 01:38:19 +0200

    Hice unos cuantos cambis

--- app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/CrearCitaActivity.kt
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/CrearCitaActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.recyclerview.app_citas_medicas.R
+import kotlinx.android.synthetic.main.activity_crear_cita.*
 
 class CrearCitaActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_cita)
@@ -17,6 +19,8 @@ class CrearCitaActivity : AppCompatActivity() {
 
 
         // Mensaje para cuando se cree la cita
-
+        bntGuardarCita.setOnClickListener{
+            Toast.makeText(this, "Todavia No se implementa, gay!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
