fba8214d169d9945115ca9e340c3370bbc73c5e8
aldairDiazLopez97 <aldair14101997@gmail.com>
2021-08-25 18:50:09 +0200

    Hice unos cuantos cambis

--- app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/PerfilPacienteActivity.kt
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/PerfilPacienteActivity.kt
@@ -2,9 +2,23 @@ package com.recyclerview.app_citas_medicas.view.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.fragment_home.*
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.recyclerview.app_citas_medicas.R
 
 class PerfilPacienteActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+    lateinit var nombres: TextView
+    lateinit var apellidos: TextView
+    lateinit var dniPaciente: TextView
+
+    private lateinit var editarPerfil: Button
+    private lateinit var changePass: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil_paciente)
@@ -16,8 +30,25 @@ class PerfilPacienteActivity : AppCompatActivity() {
 
         // Pasar el valor del dni
         val dni: String? = intent.getStringExtra("dni")
-        tvDniPaciente.text= dni
 
+        nombres = findViewById(R.id.tvNombresPaciente)
+        apellidos = findViewById(R.id.tvApellidosPaciente)
+        dniPaciente = findViewById(R.id.tvDniPaciente)
+        dniPaciente.text= dni
 
+        db.collection("usuarios").document(dniPaciente.text.toString()).get().addOnSuccessListener {
+            nombres.text = it.get("nombres").toString()
+            apellidos.text = it.get("apellidos").toString()
+        }
+
+        editarPerfil = findViewById(R.id.editarDatosPaciente)
+        changePass = findViewById(R.id.editarContraseñaPaciente)
+
+        editarPerfil.setOnClickListener{
+            Toast.makeText(this, "No tiene permiso", Toast.LENGTH_SHORT).show()
+        }
+        changePass.setOnClickListener{
+            Toast.makeText(this, "No tiene permiso", Toast.LENGTH_SHORT).show()
+        }
     }
 }
