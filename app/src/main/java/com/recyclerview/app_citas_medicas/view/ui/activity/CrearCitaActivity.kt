3160e95b4b3ef802f136622b03bb65bb9cb6574f
Sergio Avalos <45866257+WehSing@users.noreply.github.com>
2021-08-25 08:23:49 -0500

    spinner para cita

--- app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/CrearCitaActivity.kt
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/activity/CrearCitaActivity.kt
@@ -1,26 +1,56 @@
 package com.recyclerview.app_citas_medicas.view.ui.activity
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.recyclerview.app_citas_medicas.R
 import kotlinx.android.synthetic.main.activity_crear_cita.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class CrearCitaActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+    var datalista=ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_cita)
 
-        // Para el toolbar Crear Citas Medicas
-        val actionBar = supportActionBar
-        actionBar!!.title = "Crear Cita"
-        actionBar.setDisplayHomeAsUpEnabled(true)
+        db.collection("especialidad")
+            .addSnapshotListener{querySnapshot,error->
+                if(error!=null){
+                    mensaje(error.message!!)
+                    return@addSnapshotListener
+                }
+                for(document in querySnapshot!!){
+                    var cadena=" ${document.getString("Nombre")}"
+                    datalista.add(cadena)
+                }
+                EspSpinner.adapter=ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datalista)
+                }
+        }
+
+    private fun mensaje(s: String) {
+        AlertDialog.Builder(this).setTitle("Atencion")
+            .setMessage(s)
+            .setPositiveButton("OK"){d,i->}
+            .show()
+    }
+
+
+    // Para el toolbar Crear Citas Medicas
+        //val actionBar = supportActionBar
+        //actionBar!!.title = "Crear Cita"
+        //actionBar.setDisplayHomeAsUpEnabled(true)
 
 
         // Mensaje para cuando se cree la cita
-        bntGuardarCita.setOnClickListener{
-            Toast.makeText(this, "Todavia No se implementa, gay!", Toast.LENGTH_SHORT).show()
-        }
-    }
+        //bntGuardarCita.setOnClickListener{
+        //    Toast.makeText(this, "Todavia No se implementa, gay!", Toast.LENGTH_SHORT).show()
+        //}
+
 }
---
+++ app/src/main/java/com/recyclerview/app_citas_medicas/view/ui/model/Especialidad.kt
@@ -0,0 +1,5 @@
+package com.recyclerview.app_citas_medicas.view.ui.model
+
+class Especialidad {
+    var name:String?=null
+}
